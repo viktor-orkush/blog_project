2a70fbfa5bc94b011f6c5978a85e9884eacec42b
victor <viktor.orkush@gmail.com>
2018-10-11 21:20:40 +0300

    Create models Post and Comments

---
+++ blog/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.CharField('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_data = models.DateTimeField(default=timezone.now())
+    publish_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.publish_date = timezone.now()
+        self.save()
+
+    def approve_comment(self):
+        return self.comments.filter(approve_comment=True)
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs = {'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approve_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approve_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.text
